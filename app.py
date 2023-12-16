6adb167b1bd448c5e51a1571e842a2aeaa89223d
James Swank <jmswank7@gmail.com>
2023-12-15 18:25:29 -0700

    starts server

--- app.py
+++ app.py
@@ -1,2 +1,57 @@
 import pandas as pd
+import numpy as np
+from dash import Dash, html, dcc, Input, Output, State, ctx, dash_table
+import dash_bootstrap_components as dbc
+from datetime import date
+
+
+
+
+from utilities import (
+    get_grocery_stores
+)
+
+df = get_grocery_stores()
+
+
+app = Dash(__name__, suppress_callback_exceptions=True, external_stylesheets=[dbc.themes.DARKLY])
+
+header = html.Div("Arapahoe County Food Deserts", className="h2 p-2 text-white bg-primary text-center")
+
+bgcolor = "#f3f3f1"  # mapbox light map land color
+
+template = {"layout": {"paper_bgcolor": bgcolor, "plot_bgcolor": bgcolor}}
+
+theme = {
+    'dark': True,
+    'detail': '#007439',
+    'primary': '#00EA64',
+    'secondary': '#6E6E6E',
+}
+
+def blank_fig(height):
+    """
+    Build blank figure with the requested height
+    """
+    return {
+        "data": [],
+        "layout": {
+            "height": height,
+            "template": template,
+            "xaxis": {"visible": False},
+            "yaxis": {"visible": False},
+        },
+    }
+
+
+
+app.layout = dbc.Container([
+    header,
+])
+
+
+
+if __name__ == "__main__":
+    app.run_server(debug=True, port=8080)
+
 
---
+++ utilities.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+
+
+groceries = pd.read_csv('/Users/jamesswank/Downloads/Grocery_S_ExportTable1.csv')
+
+def get_grocery_stores():
+    df = groceries
+
+
+
+    return df
+
